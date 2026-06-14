0419e7975bd0b2b85bec7fb1559509f48b0ca1b3
shivani Mourya <shivanimouryaa2@gmail.com>
2026-06-14 14:45:09 +0530

    second

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 let express = require("express");
 let cors = require("cors");
 
@@ -7,4 +8,5 @@ app.use(cors());
 app.use(express.json());
 app.use("/",bookingRoutes);
 
+let PORT = process.env.PORT || 9000
 app.listen(9000,()=>console.log("Server is running on port 9000"));
